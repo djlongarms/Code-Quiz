1a08465ee4c3d53ab530c6ee81b9e876ecfcb994
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-18 21:10:28 -0800

    added and implemented correct answers array

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Array holding questions for quiz and an identifier for the current question
 let questions = [
-  'What is your name?',
-  'What is your quest?',
+  'Which line would print the phrase "Hello World!" to the console?',
+  'Which of these',
   'What is your favorite color?',
   'What is the capital of Assyria?',
   'What is the airspeed velocity of an unladen swallow?'
@@ -9,15 +9,24 @@ let questions = [
 let current = 0
 
 // A boolean to see if the quiz has been finished
-let quizOver = false
+let quizFinished = false
 
 // Array of arrays for holding answers to each question
 let answers = [
-  ['1.1', '1.2', '1.3', '1.4'],
+  ['console.Log("Hello World!")', 'console.log(Hello World!)', 'Hello World!', 'console.log=Hello World!'],
   ['2.1', '2.2', '2.3', '2.4'],
   ['3.1', '3.2', '3.3', '3.4'],
   ['4.1', '4.2', '4.3', '4.4'],
   ['5.1', '5.2', '5.3', '5.4']
+]
+
+// Array with the correct answers for each 
+let correctAnswers = [
+  'console.Log("Hello World!")',
+  '',
+  '',
+  '',
+  ''
 ]
 
 // A counter for timing the quiz
@@ -28,9 +37,10 @@ document.getElementById('start').addEventListener('click', () => {
   // Hides introductory text and reveals elements for quiz usage
   document.getElementById('introduction').classList.add('hide')
   document.getElementById('quiz').classList.remove('hide')
+  document.getElementById('answers').classList.remove('hide')
 
   // Sets counter to starting time and sets "Timer" text to include counter
-  counter = 5
+  counter = 75
   document.getElementById('timer').textContent = `Timer: ${counter}`
 
   // Resets current to 0 in case the user is retrying the quiz
@@ -47,16 +57,23 @@ document.getElementById('start').addEventListener('click', () => {
 
   // Creates interval for "Timer"
   let timer = setInterval(() => {
-    // Checks if time is up
-    if(counter <= 0) {
-      // Ends interval and hides the quiz materials, then brings up "Game Over" screen
+    // Checks if user finished the quiz
+    if (quizFinished) {
+      quizFinished = false
       clearInterval(timer)
-      document.getElementById('quiz').classList.add('hide')
-      document.getElementById('gameOver').classList.remove('hide')
+
     } else {
-      // If time isn't up, decrements counter then sets "Timer" with new time left
-      counter--;
-      document.getElementById('timer').textContent = `Timer: ${counter}`
+      // Checks if time is up
+      if(counter <= 1) {
+        // Ends interval and hides the quiz materials
+        clearInterval(timer)
+        document.getElementById('quiz').classList.add('hide')
+        document.getElementById('answers').classList.add('hide')
+      } else {
+        // If time isn't up, decrements counter then sets "Timer" with new time left
+        counter--;
+        document.getElementById('timer').textContent = `Timer: ${counter}`
+      }
     }
   }, 1000)
 })
@@ -65,6 +82,12 @@ document.getElementById('start').addEventListener('click', () => {
 document.addEventListener('click', event => {
   // Checks to see if clicked element is an "answer" button
   if(event.target.classList.contains('answer')) {
+    // Checks if the answer chosen was correct
+    if (event.target.textContent !== correctAnswers[current]) {
+      counter -= 15
+      document.getElementById('timer').textContent = `Timer: ${counter}`
+    }
+
     // Increments "current" to move to the next question
     current++
 
@@ -82,7 +105,9 @@ document.addEventListener('click', event => {
     else {
       // Hides the quiz display
       document.getElementById('quiz').classList.add('hide')
+      document.getElementById('answers').classList.add('hide')
       document.getElementById('introduction').classList.remove('hide')
+      quizFinished = true
     }
   }
 })
